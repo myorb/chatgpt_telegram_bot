924b7c69ba0d0011c25d4d727f0faa7cf3cbf54a
Alex Shalaiev <itdep24@gmail.com>
2024-09-05 00:36:28 +0200

    add envs

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,14 @@
 import yaml
 import dotenv
 from pathlib import Path
+import os
+
+telegram_token = os.environ.get("telegram_token")
+openai_api_key = os.environ.get("openai_api_key")
+mongodb_uri = os.environ.get("MONGODB_URL")
+
+if mongodb_uri is None:
+    raise ValueError("Please set the environment variable 'telegram_token'") 
 
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
@@ -9,11 +17,14 @@ with open(config_dir / "config.yml", 'r') as f:
     config_yaml = yaml.safe_load(f)
 
 # load .env config
-config_env = dotenv.dotenv_values(config_dir / "config.env")
+# config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
+# telegram_token = config_yaml["telegram_token"]
+# openai_api_key = config_yaml["openai_api_key"]
+# mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
+
+
 openai_api_base = config_yaml.get("openai_api_base", None)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
@@ -21,7 +32,6 @@ enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 image_size = config_yaml.get("image_size", "512x512")
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
